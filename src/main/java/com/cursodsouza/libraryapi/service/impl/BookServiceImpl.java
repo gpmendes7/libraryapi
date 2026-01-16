9ccce22f49d916db173b85b856002611690eeca7
gpmendes7 <andrewsseven@gmail.com>
2026-01-16 13:11:16 -0300

    commit

--- src/main/java/com/cursodsouza/libraryapi/service/impl/BookServiceImpl.java
+++ src/main/java/com/cursodsouza/libraryapi/service/impl/BookServiceImpl.java
@@ -4,6 +4,10 @@ import com.cursodsouza.libraryapi.exception.BusinessException;
 import com.cursodsouza.libraryapi.model.entity.Book;
 import com.cursodsouza.libraryapi.model.repository.BookRepository;
 import com.cursodsouza.libraryapi.service.BookService;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -44,6 +48,17 @@ public class BookServiceImpl implements BookService {
             throw new IllegalArgumentException("Book id cant be null.");
         }
         return this.repository.save(book);
+    }
+
+    @Override
+    public Page<Book> find(Book filter, Pageable pageRequest) {
+        Example<Book> example = Example.of(filter,
+                ExampleMatcher
+                        .matching()
+                        .withIgnoreCase()
+                        .withIgnoreNullValues()
+                        .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING));
+        return repository.findAll(example, pageRequest);
     }
 
 
